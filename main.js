2b8499bdd9420e0a56b0d0be43f84e79f22c3917
Riley Jackson Sardella <Riley.sardella@stu.ocsb.ca>
2024-10-17 10:43:33 -0400

    Yeet

--- main.js
+++ main.js
@@ -9,12 +9,49 @@
 'use strict';
 
 /*** Event Listeners ***/
-
-
+document.getElementById("d4").addEventListener("click", roll_d4)
+document.getElementById("d6").addEventListener("click", roll_d6)
+document.getElementById("d8").addEventListener("click", roll_d8)
+document.getElementById("d10").addEventListener("click", roll_d10)
+document.getElementById("d12").addEventListener("click", roll_d12)
+document.getElementById("d20").addEventListener("click", roll_d20)
+document.getElementById("d100").addEventListener("click", roll_d100)
+document.getElementById("enter").addEventListener("click", greeting)
 /*** Functions ***/
 
 // Return a random integer from `min` to `max`, inclusive
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+function roll_d4(){
+    let roll = randInt(1, 4)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d6(){
+    let roll = randInt(1, 6)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d8(){
+    let roll = randInt(1, 8)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d10(){
+    let roll = randInt(1, 10)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d12(){
+    let roll = randInt(1, 12)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d20(){
+    let roll = randInt(1, 20)
+    document.getElementById("die_roll").textContent = roll;
+}
+function roll_d100(){
+    let roll = randInt(1, 100)
+    document.getElementById("die_roll").textContent = roll;
+}
+function greeting(){
+    let name = document.getElementById("user_input").value
+    document.getElementById("greeting").textContent = `Hello ${name}`
+}
